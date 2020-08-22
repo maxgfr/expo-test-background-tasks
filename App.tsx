41139c37131d86586044c8c69ff2f6d92329ded3
@maxgfr <maxgolfier@gmail.com>
2020-08-22 23:19:59 +0200

    push

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { registerFetchTask } from "./tasks";
-const INTERVAL_TASKS = 60 * 15;
+const INTERVAL_TASKS = 60 * 1;
 
 registerFetchTask(
   "TASK_NAME",
